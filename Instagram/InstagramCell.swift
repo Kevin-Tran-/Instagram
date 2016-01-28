f7963053b53bbc30238f2c851947444eb0cfa2ca
Kevin-Tran- <tranpd88@gmail.com>
2016-01-27 19:04:30 -0800

    Instagram stage 2

---
+++ Instagram/InstagramCell.swift
@@ -0,0 +1,25 @@
+//
+//  InstagramCell.swift
+//  Instagram
+//
+//  Created by Kevin Tran on 1/27/16.
+//  Copyright © 2016 Kevin Tran. All rights reserved.
+//
+
+import UIKit
+
+class InstagramCell: UITableViewCell {
+
+    @IBOutlet weak var instagramPic: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
